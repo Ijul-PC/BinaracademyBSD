1a50c103691bb592bde6c7ccad754260edd7f8a2
Ijul-Pc <azulmecha@gmail.com>
2018-10-24 20:02:04 +0700

    coba

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    }
+    public static void main (String[] args){
+        /*
+        Coding pertama kali
+         */
+
+        System.out.println("Hello World"); //print line (pindah baris)
+        System.out.print("binar\nacademy ");
+        System.out.print("bsd");
+        System.out.println();
+        newMain();
+    }
+
+    public static void newMain(){
+        System.out.println("coba coba");
+    }
+
+
+}
+
+pubpoc
